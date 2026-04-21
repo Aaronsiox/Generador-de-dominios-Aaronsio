55eefd8880314d4b3a2c9baad7634acd877b671e
Aaronsiox <jhoyamp@gmail.com>
2026-04-21 01:42:26 +0000

    Primer test

--- src/app.js
+++ src/app.js
@@ -6,6 +6,26 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+let pronoun = ['the', 'our'];
+    let adj = ['great', 'big'];
+    let noun = ['jogger', 'racoon'];
+
+    const button = document.getElementById('btn-generate');
+    const domainList = document.getElementById('domain-list');
+    
+    button.addEventListener('click', () => {
+        domainList.innerHTML = '';
+        for (let p of pronoun) {
+        for (let a of adj) {
+        for (let n of noun) {
+
+                  let domainName = `${p}${a}${n}.com`;
+                let li = document.createElement('li');
+                li.innerText = domainName;
+                domainList.appendChild(li);
+                console.log(domainName);
+                }
+            }
+        }
+    });
 };
